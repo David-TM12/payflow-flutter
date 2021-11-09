33f068c6ce9538d988c4daf4746fda098dc56f90
DavidTM <deivi_gf144@hotmail.com>
2021-11-09 12:01:39 -0400

    Criando LoginPage, image de Person, Mini logo, Title, SocialLoginButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
 void main() {
@@ -12,7 +12,7 @@ class AppWidget extends StatelessWidget {
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(primaryColor: AppColors.primary),
-      home: SplashPage(),
+      home: LoginPage(),
     );
   }
 }
