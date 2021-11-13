56a7c0569ba1b47743a52c58811747da75427fca
DavidTM <deivi_gf144@hotmail.com>
2021-11-13 16:24:30 -0400

    utilizando sharedPreferences e salvando usuario logado

--- lib/shared/auth/auth_controller.dart
+++ lib/shared/auth/auth_controller.dart
@@ -1,15 +1,14 @@
-
 import 'package:flutter/material.dart';
-import 'package:payflow/modules/home/home_page.dart';
-import 'package:payflow/modules/login/login_page.dart';
+import 'package:payflow/shared/models/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
   var _isAuthenticated = false;
-  var _user;
+  UserModel? _user;
 
-  get user => _user;
+  UserModel get user => _user!;
 
-  void setUser(BuildContext context, var user) {
+  void setUser(BuildContext context, UserModel? user) {
     if (user != null) {
       _user = user;
       _isAuthenticated = true;
@@ -18,5 +17,18 @@ class AuthController {
       _isAuthenticated = false;
       Navigator.pushReplacementNamed(context, "/login");
     }
+  }
+
+  Future<void> saveUser(UserModel user) async {
+    final instance = await SharedPreferences.getInstance();
+    await instance.setString("user", user.toJson());
+    return;
+  }
+
+  Future<void> currentUser(BuildContext context) async {
+    final instance = await SharedPreferences.getInstance();
+    final json = instance.get("user") as String;
+    setUser(context, UserModel.fromJson(json));
+    return;
   }
 }
