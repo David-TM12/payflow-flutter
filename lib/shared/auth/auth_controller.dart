7bba67cd41c4d1bfd0772911365cf5d97021400a
DavidTM <deivi_gf144@hotmail.com>
2021-11-13 16:51:37 -0400

    alterando a logica authcontroller para usuario logado e sistema de autenticação finalizado

--- lib/shared/auth/auth_controller.dart
+++ lib/shared/auth/auth_controller.dart
@@ -3,18 +3,16 @@ import 'package:payflow/shared/models/user_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
-  var _isAuthenticated = false;
   UserModel? _user;
 
   UserModel get user => _user!;
 
   void setUser(BuildContext context, UserModel? user) {
     if (user != null) {
+      saveUser(user);
       _user = user;
-      _isAuthenticated = true;
       Navigator.pushReplacementNamed(context, "/home");
     } else {
-      _isAuthenticated = false;
       Navigator.pushReplacementNamed(context, "/login");
     }
   }
@@ -27,8 +25,13 @@ class AuthController {
 
   Future<void> currentUser(BuildContext context) async {
     final instance = await SharedPreferences.getInstance();
-    final json = instance.get("user") as String;
-    setUser(context, UserModel.fromJson(json));
-    return;
+    await Future.delayed(Duration(seconds: 2));
+    if (instance.containsKey("user")) {
+      final json = instance.get("user") as String;
+      setUser(context, UserModel.fromJson(json));
+      return;
+    } else {
+      setUser(context, null);
+    }
   }
 }
