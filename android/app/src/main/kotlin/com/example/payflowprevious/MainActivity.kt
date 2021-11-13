e0af2b215b5ebdfedfe1cbd885382ef379734486
DavidTM <deivi_gf144@hotmail.com>
2021-11-13 15:25:40 -0400

    adicionando login com google e corrigindo config firebase

--- android/app/src/main/kotlin/com/example/payflowprevious/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/payflowprevious/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.payflowprevious
+package com.example.payflow
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,12 +1,11 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_style.dart';
 import 'package:payflow/shared/widgets/social_login/social_login_button.dart';
 
-class LoginPage extends StatefulWidget{
+class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
 
   @override
@@ -14,6 +13,7 @@ class LoginPage extends StatefulWidget{
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -26,51 +26,50 @@ class _LoginPageState extends State<LoginPage> {
           children: [
             Container(
               width: size.width,
-              height: size.height * 0.3,         
+              height: size.height * 0.3,
               color: AppColors.primary,
             ),
             Positioned(
-              top: 40,
-              left: 0, 
-              right: 0, 
-              child: Image.asset(
-                AppImages.person, 
-                width: 208, 
-                height: 300,
-              )
-            ),
+                top: 40,
+                left: 0,
+                right: 0,
+                child: Image.asset(
+                  AppImages.person,
+                  width: 208,
+                  height: 300,
+                )),
             Positioned(
-              bottom: size.height * 0.15,
-              left: 0,
-              right: 0,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset(AppImages.logomini),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 30, left: 70, right: 70),
-                    child: Text(
-                      "Organize seus boletos em um só lugar",
-                      textAlign: TextAlign.center,
-                      style: TextStyles.titleHome,
+                bottom: size.height * 0.15,
+                left: 0,
+                right: 0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(AppImages.logomini),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 30, left: 70, right: 70),
+                      child: Text(
+                        "Organize seus boletos em um só lugar",
+                        textAlign: TextAlign.center,
+                        style: TextStyles.titleHome,
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 40, left: 40, right: 40),
-                    child: SocialLoginButton(
-                      onTap: (){
-                        print("Clicou");
-                      },
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 40, left: 40, right: 40),
+                      child: SocialLoginButton(
+                        onTap: () {
+                          controller.googleSignIn(context);
+                        },
+                      ),
                     ),
-                  ),
-                ],
-              )
-            )
+                  ],
+                ))
           ],
         ),
       ),
     );
   }
-
 }
