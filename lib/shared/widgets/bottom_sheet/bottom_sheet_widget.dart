72823590258fdc05fc1e25f3566c2d90d533138f
DavidTM <deivi_gf144@hotmail.com>
2021-11-14 00:23:59 -0400

    Adicionando RotateBox e SafeArea no bottom_sheet

--- lib/shared/widgets/bottom_sheet/bottom_sheet_widget.dart
+++ lib/shared/widgets/bottom_sheet/bottom_sheet_widget.dart
@@ -10,6 +10,7 @@ class BottomSheetWidget extends StatelessWidget {
   final VoidCallback secondaryOnPressed;
   final String title;
   final String subtitle;
+
   const BottomSheetWidget(
       {Key? key,
       required this.primaryLabel,
@@ -22,47 +23,52 @@ class BottomSheetWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-        color: AppColors.shape,
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                color: Colors.black.withOpacity(0.6),
-              ),
-            ),
-            Column(
+    return SafeArea(
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Material(
+          child: Container(
+            color: AppColors.shape,
+            child: Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(40),
-                  child: Text.rich(
-                    TextSpan(
-                        text: title,
-                        style: TextStyles.buttonBoldHeading,
-                        children: [
-                          TextSpan(
-                            text: "\n$subtitle",
-                            style: TextStyles.buttonHeading,
-                          ),
-                        ]),
-                    textAlign: TextAlign.center,
+                Expanded(
+                  child: Container(
+                    color: Colors.black.withOpacity(0.6),
                   ),
                 ),
-                Container(
-                  height: 1,
-                  color: AppColors.stroke,
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(40),
+                      child: Text.rich(
+                        TextSpan(
+                            text: title,
+                            style: TextStyles.buttonBoldHeading,
+                            children: [
+                              TextSpan(
+                                text: "\n$subtitle",
+                                style: TextStyles.buttonHeading,
+                              ),
+                            ]),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Container(
+                      height: 1,
+                      color: AppColors.stroke,
+                    ),
+                    SetLabelButtons(
+                      enablePrimaryColor: true,
+                      primaryLabel: primaryLabel,
+                      primaryOnPressed: primaryOnPressed,
+                      secondaryLabel: secondaryLabel,
+                      secondaryOnPressed: secondaryOnPressed,
+                    )
+                  ],
                 ),
-                SetLabelButtons(
-                  enablePrimaryColor: true,
-                  primaryLabel: primaryLabel,
-                  primaryOnPressed: primaryOnPressed,
-                  secondaryLabel: secondaryLabel,
-                  secondaryOnPressed: secondaryOnPressed,
-                )
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
