168f46de7148c36470a8de52608dab26dd637235
DavidTM <deivi_gf144@hotmail.com>
2021-11-13 23:11:44 -0400

    Adicionando RotateBox no app

--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -15,39 +15,50 @@ class BarcodeScannerPage extends StatefulWidget {
 class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          "Escaneie o código de barras do boleto",
-          style: TextStyles.buttonBackground,
+    return SafeArea(
+      top: true,
+      bottom: true,
+      left: true,
+      right: true,
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            title: Text(
+              "Escaneie o código de barras do boleto",
+              style: TextStyles.buttonBackground,
+            ),
+            centerTitle: true,
+            leading: BackButton(
+              color: AppColors.background,
+            ),
+          ),
+          body: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.black.withOpacity(0.5),
+                ),
+              ),
+              Expanded(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.transparent,
+                  )),
+              Expanded(
+                  child: Container(
+                color: Colors.black.withOpacity(0.5),
+              ))
+            ],
+          ),
+          bottomNavigationBar: SetLabelButtons(
+            primaryLabel: "Inserir código do boleto",
+            primaryOnPressed: () {},
+            secondaryLabel: "Adicionar da galeria",
+            secondaryOnPressed: () {},
+          ),
         ),
-        leading: BackButton(
-          color: AppColors.background,
-        ),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-              child: Container(
-            color: Colors.black,
-          )),
-          Expanded(
-              flex: 2,
-              child: Container(
-                color: Colors.transparent,
-              )),
-          Expanded(
-              child: Container(
-            color: Colors.black,
-          ))
-        ],
-      ),
-      bottomNavigationBar: SetLabelButtons(
-        primaryLabel: "Inserir código do boleto",
-        primaryOnPressed: () {},
-        secondaryLabel: "Adicionar da galeria",
-        secondaryOnPressed: () {},
       ),
     );
   }
